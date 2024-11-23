a4acdb854a20e97f415e302f7c88401d1c3945e4
Eni Bondarenko <bondarenkoenny@gmail.com>
2024-11-23 13:05:18 +0100

    add second argument to generateHtml; create individual js folders for generateHtml function(lib.js) and tasks array(data.js)

---
+++ data.js
@@ -0,0 +1,18 @@
+const tasks = [
+    {
+        name: "Task 1",
+        active: true
+    },
+    {
+        name: "Task 2",
+        active: false
+    },
+    {
+        name: "Task 3",
+        active: true
+    },
+    {
+        name: "Task 4",
+        active: true
+    }
+];
---
+++ lib.js
@@ -0,0 +1,24 @@
+ 
+        //we determine parameter when we call function
+        //Generates html
+        const generateHtml = (tasks, rootElementId) => {
+            let html = "";
+
+            // loop generates html for each task
+            for (let i = 0; i < tasks.length; i++) {
+                let task = tasks[i]; // individual object
+                html = html +
+                    `<li class="task">
+                <input id=task-${i} type="checkbox" name="completed" ${task.active ? "" : "checked"}>
+                <label for="task-${i}">${task.name}</label>
+                <button class="deleteBtn">Delete</button>
+                <button class="editBtn">Edit</button>
+            </li>`
+            }
+
+            // <ul id="task"></ul> parent element of task lists
+           document.getElementById(rootElementId).innerHTML = html;
+        }
+
+        // second parameter: element where we want to load html.
+        generateHtml(tasks, "tasks");
